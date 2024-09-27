cc0e74a2de4023a01a9a7b2f9a495391f1591588
Allen Chrisso Dhas <allencdvja@gmail.com>
2024-09-27 11:07:05 +0530

    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[rgba(120,119,198,0.3)] `}
       >
         <div className="fixed h-full w-full bg-black"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f35_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f35_1px,transparent_1px)] bg-[size:50px_50px] [mask-image:radial-gradient(ellipse_80%_80%_at_50%_20%,#000_20%,transparent_100%)]">
         <div className=" top-0 z-[-2] h-screen w-screen  bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div></div></div>
